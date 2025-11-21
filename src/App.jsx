c76d1fa654d89cb36deab04ca0b7dee927eedb85
fabioluisstein <fabio.l.stein@gmail.com>
2025-11-21 09:06:03 -0300

    Update .gitignore to properly ignore node_modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,2 @@
-import GarageLotteryApp from "./sorteio_garagens_flor_de_lis_lado_a_lado_compacto_v3_hide_reservas.jsx";
+import GarageLotteryApp from "./sorteio_garagens_flor_de_lis_lado_a_lado_compacto_v4_hide_reservas.jsx";
 export default function App() { return <GarageLotteryApp />; }
