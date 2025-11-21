5ed76c5cca82193207a21e12fc46c474d21f7581
fabioluisstein <fabio.l.stein@gmail.com>
2025-11-21 09:12:27 -0300

    Renomeia arquivo principal para SorteioGaragens.jsx e atualiza referências

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,2 @@
-import GarageLotteryApp from "./sorteio_garagens_flor_de_lis_lado_a_lado_compacto_v4_hide_reservas.jsx";
-export default function App() { return <GarageLotteryApp />; }
+import SorteioGaragens from "./SorteioGaragens.jsx";
+export default function App() { return <SorteioGaragens />; }
